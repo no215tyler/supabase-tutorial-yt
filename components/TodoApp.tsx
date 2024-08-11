b1a23dd6b0901f7f087798a250ae80587dd01d4c
no215tyler <no.215.tyler@gmail.com>
2024-08-11 22:38:24 +0900

    ✨ addTodoの実装

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getAllTodos } from '@/utils/supabaseFunction';
+import { addTodo, getAllTodos } from '@/utils/supabaseFunction';
 import TodoList from './TodoList';
 
 const TodoApp = () => {
   const[ todos, setTodos ] = useState<any>([]);
+  const[ title, setTitle ] = useState<string>('');
 
   useEffect(() => {
     const getTodos = async () => {
@@ -14,11 +15,24 @@ const TodoApp = () => {
     getTodos();
   }, []);
 
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (title === '') return;
+
+    //Todoの追加
+    await addTodo(title);
+    let todo = await getAllTodos();
+    setTodos(todo);
+
+    setTitle('');
+  };
+
   return <section className='text-center mb-2 text-2xl font-medium'>
     <h3>Supabase Todo App</h3>
-    <form>
-      <input type='text' className='mr-2 shadow-lg p-1 outline-none' />
-      <button className='shadow-md border-2 p-1 rounded-lg bg-green-200'>Add</button>
+    <form onSubmit={(e) => handleSubmit(e)} >
+      <input type='text' className='text-black mr-2 shadow-lg p-1 outline-none' onChange={(e) => setTitle(e.target.value)} value={title} />
+      <button className='text-black shadow-md border-2 p-1 rounded-lg bg-green-200'>Add</button>
     </form>
     <TodoList todos={todos}/>
   </section>
